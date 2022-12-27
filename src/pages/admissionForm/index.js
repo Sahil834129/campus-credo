50d6555c8349345f345e8b1ed2a8dd07469d9fa3
Anand <anandvishwakarma83@gmail.com>
2022-12-27 14:25:03 +0530

    annual family income added on student details page, obtained marks and marks details added, review admission dialog chnages to keep separate accordion for extracurriular and backfround check and will be displayed when age is above 11

--- src/pages/admissionForm/index.js
+++ src/pages/admissionForm/index.js
@@ -1,182 +1,209 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import {
-  Container,
-  Row,
-  Col,
   Button,
-  Form as BootStrapForm
-} from 'react-bootstrap'
-import Layout from '../../common/layout'
-import Breadcrumbs from '../../common/Breadcrumbs'
-import LeftMenuBar from '../../common/LeftMenuBar'
-import PageContent from '../../resources/pageContent'
-import 'react-datepicker/dist/react-datepicker.css'
+  Col,
+  Container,
+  Form as BootStrapForm,
+  Row,
+} from "react-bootstrap";
+import "react-datepicker/dist/react-datepicker.css";
+import Breadcrumbs from "../../common/Breadcrumbs";
+import Layout from "../../common/layout";
+import LeftMenuBar from "../../common/LeftMenuBar";
+import PageContent from "../../resources/pageContent";
 
-import { useSelector } from 'react-redux'
-import AddChildDialog from '../../dialogs/addChild'
-import StudentDetails from './student-details'
-import MedicalForm from './medicalForm'
-import ExtracurricularForm from './extracurriculars'
-import BackgroundCheckForm from './background-check'
-import ParentsGuardianComponent from './parents-guardian'
-import SupportingDocumentForm from './supportingdocumentform'
-import { getChildAge } from '../../utils/helper'
+import { useSelector } from "react-redux";
+import AddChildDialog from "../../dialogs/addChild";
+import { getChildAge } from "../../utils/helper";
+import BackgroundCheckForm from "./background-check";
+import ExtracurricularForm from "./extracurriculars";
+import MedicalForm from "./medicalForm";
+import ParentsGuardianComponent from "./parents-guardian";
+import StudentDetails from "./student-details";
+import SupportingDocumentForm from "./supportingdocumentform";
 
 export const AdmissionForms = () => {
-  const [currentStudent, setCurrentStudent] = useState({})
+  const [currentStudent, setCurrentStudent] = useState({});
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const studentInitialValue = {
-    childId: '',
-    firstName: '',
-    middleName: '',
-    lastName: '',
-    className: '',
-    dateOfBirth: '',
-    isProvidingCurrentSchoolInfo: 'No',
+    childId: "",
+    firstName: "",
+    familyIncome: "",
+    middleName: "",
+    lastName: "",
+    className: "",
+    dateOfBirth: "",
+    isProvidingCurrentSchoolInfo: "No",
     transportFacility: false,
-    schoolName: '',
-    schoolBoard: '',
-    obtainedMarks: '',
-    schoolAddressLine1: '',
-    schoolAddressLine2: '',
-    schoolCity: '',
-    schoolState: '',
-    schoolPincode: '',
-    gender: '',
-    category: '',
-    identificationMarks: '',
-    religion: '',
-    nationality: '',
-    addressLine1: '',
-    addressLine2: '',
-    pincode: '',
-    city: '',
-    state: '',
-    boardingFacility: false
-  }
-  const childsList = useSelector(state => state.childsData.childs)
-  const [showAddChildDialog, setShowAddChildDialog] = useState(false)
-  const [showStudentList, setShowStudentList] = useState(false)
-  const [pageTitle, setPageTitle] = useState('')
-  const [step, setStep] = useState(1)
-  const [selectedChild, setSelectedChild] = useState(studentInitialValue)
-  const [sidebarMenuItems, setSidebarMenuItems] = useState(PageContent.ADMISSION_FORM_SIDEBAR_MENU_ITEMS)
+    schoolName: "",
+    schoolBoard: "",
+    obtainedMarks: "",
+    maxMarks: "",
+    marksInPercentage: "",
+    schoolAddressLine1: "",
+    schoolAddressLine2: "",
+    schoolCity: "",
+    schoolState: "",
+    markingScheme: "",
+    schoolPincode: "",
+    gender: "",
+    category: "",
+    hasIdentificationMarks: "No",
+    identificationMarks: "",
+    religion: "",
+    nationality: "",
+    addressLine1: "",
+    addressLine2: "",
+    pincode: "",
+    city: "",
+    state: "",
+    boardingFacility: false,
+    grade: "",
+  };
+  const childsList = useSelector((state) => state.childsData.childs);
+  const [showAddChildDialog, setShowAddChildDialog] = useState(false);
+  const [showStudentList, setShowStudentList] = useState(false);
+  const [pageTitle, setPageTitle] = useState("");
+  const [step, setStep] = useState(1);
+  const [selectedChild, setSelectedChild] = useState(studentInitialValue);
+  const [sidebarMenuItems, setSidebarMenuItems] = useState(
+    PageContent.ADMISSION_FORM_SIDEBAR_MENU_ITEMS
+  );
 
-  const handleChildSelection = childId => {
+  const handleChildSelection = (childId) => {
     const selectedChildObj = childsList.find(
-      it => it.childId === parseInt(childId)
-    )
-    setCurrentStudent({ ...selectedChildObj })
+      (it) => it.childId === parseInt(childId)
+    );
+    setSelectedChild({ ...selectedChildObj, ...studentInitialValue });
+    setCurrentStudent({ ...selectedChildObj });
     if (selectedChildObj) {
-      let childobj = selectedChildObj
-      let selectedChildCopy = JSON.parse(JSON.stringify(studentInitialValue))
+      let childobj = selectedChildObj;
+      let selectedChildCopy = JSON.parse(JSON.stringify(studentInitialValue));
       selectedChildCopy = {
         ...selectedChildCopy,
-        ...childobj
-      }
-      setSelectedChild(selectedChildCopy)
+        ...childobj,
+        hasIdentificationMarks:
+          childobj.identificationMarks !== "" ? "Yes" : "No",
+      };
+      setSelectedChild(selectedChildCopy);
     }
-  }
+  };
 
-  const getCurrentComponent = currentStep => {
+  const getCurrentComponent = (currentStep) => {
     switch (currentStep) {
       case 1:
-        setShowStudentList(true)
-        setPageTitle('Student Details')
-        break
+        setShowStudentList(true);
+        setPageTitle("Student Details");
+        break;
       case 2:
         //setShowStudentList(false)
-        setPageTitle('Medical Details')
-        break
+        setPageTitle("Medical Details");
+        break;
       case 3:
         //setShowStudentList(false)
-        setPageTitle('Extracurriculars')
-        break
+        setPageTitle("Extracurriculars");
+        break;
       case 4:
         //setShowStudentList(false)
-        setPageTitle('Background Check')
-        break
+        setPageTitle("Background Check");
+        break;
       case 5:
         //setShowStudentList(false)
-        setPageTitle('Parents/Guardian')
-        break
+        setPageTitle("Parents/Guardian");
+        break;
       case 6:
         //setShowStudentList(false)
-        setPageTitle('Supporting Documents')
-        break
+        setPageTitle("Supporting Documents");
+        break;
       default:
-        break
+        break;
     }
-  }
+  };
   useEffect(() => {
-    if (childsList.length > 0) {
-      let queryChildId 
-      let selectedChild
-      try {
-        queryChildId= atob(queryParams.get("childId")).replace("#", "")
-      } catch(error) {
-        console.log('Error while getting the query string')
-      }
-      
-      if (queryChildId && !isNaN(queryChildId)) {
-        selectedChild = childsList.find(
-          it => it.childId === parseInt(queryChildId)
-        )
-      }
-      if (!selectedChild)
-        selectedChild = childsList[0]
-      
-      setSelectedChild(val => {
-        return { ...val, ...selectedChild }
-      })
-      setCurrentStudent({ ...selectedChild })
-    }
-  }, [childsList])
+    let selectedChildFromList = getSelectedChildFromList();
+    setSelectedChild({ ...selectedChildFromList, studentInitialValue });
+
+    // if (childsList.length > 0) {
+    // 	setSelectedChild(val => {
+    // 		return {
+    // 			...val,
+    // 			...selectedChild,
+    // 			hasIdentificationMarks: selectedChild.identificationMarks !== '' ? 'Yes' : 'No'
+    // 		}
+    // 	})
+    // 	setCurrentStudent({ ...selectedChild, hasIdentificationMarks: selectedChild.identificationMarks !== '' ? 'Yes' : 'No' })
+    // }
+  }, [childsList]);
 
   useEffect(() => {
-    getCurrentComponent(step)
-  }, [step])
+    getCurrentComponent(step);
+  }, [step]);
 
   useEffect(() => {
-    if (currentStudent) 
-      getSidebarMenuItems()
-  }, [currentStudent])
+    if (currentStudent) getSidebarMenuItems();
+  }, [currentStudent]);
 
   function getSidebarMenuItems() {
-    let menuItems = [...sidebarMenuItems]
-    const childAge = getChildAge(selectedChild.dateOfBirth)
+    let menuItems = [...sidebarMenuItems];
+    const childAge = getChildAge(selectedChild.dateOfBirth);
     menuItems.forEach((item, index) => {
-      if (item.title === 'Extracurriculars' || item.title === 'Background Check'){
-        item.show = (childAge < 11 ? false : true)
+      if (
+        item.title === "Extracurriculars" ||
+        item.title === "Background Check"
+      ) {
+        item.show = childAge < 11 ? false : true;
       }
-    })
-    setSidebarMenuItems(menuItems)
+    });
+    setSidebarMenuItems(menuItems);
+  }
+
+  function getSelectedChildFromList() {
+    let selectedChildFromList;
+    let queryChildId;
+
+    if (!childsList || childsList.length == 0) return selectedChildFromList;
+
+    try {
+      queryChildId = atob(queryParams.get("childId")).replace("#", "");
+    } catch (error) {
+      console.log("Error while getting the query string");
+    }
+
+    if (queryChildId && !isNaN(queryChildId)) {
+      selectedChildFromList = childsList.find(
+        (it) => it.childId === parseInt(queryChildId)
+      );
+    }
+
+    if (!selectedChildFromList) selectedChildFromList = childsList[0];
+
+    return selectedChildFromList;
   }
 
   return (
     <Layout>
-      <section className='content-area'>
-        <Container className='content-area-inner pt-n16 admmission-sequence-wrap'>
-          <Col className='inner-page-content'>
-            <Row className='content-section'>
+      <section className="content-area">
+        <Container className="content-area-inner pt-n16 admmission-sequence-wrap">
+          <Col className="inner-page-content">
+            <Row className="content-section">
               <Breadcrumbs />
               {showStudentList && (
-                <div className='page-container border-bottom d-flex justify-content-between pb-2 pt-2'>
-                  <div className='row-wrapper '>
-                    <span className='selectbox'>
+                <div className="page-container border-bottom d-flex justify-content-between pb-2 pt-2">
+                  <div className="row-wrapper ">
+                    <span className="selectbox">
                       <label>
-                        Select Child <span className='req'>*</span>
+                        Select Child <span className="req">*</span>
                       </label>
-                      <div className='frm-cell'>
-                        <BootStrapForm.Group className='frm-cell'>
+                      <div className="frm-cell">
+                        <BootStrapForm.Group className="frm-cell">
                           <BootStrapForm.Select
-                            name='selectedChildId'
-                            disabled={step>1}
-                            onChange={e => {
-                              handleChildSelection(e.target.value)
+                            name="selectedChildId"
+                            disabled={step > 1}
+                            onChange={(e) => {
+                              handleChildSelection(e.target.value);
                             }}
                             value={selectedChild.childId}
                           >
@@ -184,37 +211,40 @@ export const AdmissionForms = () => {
                               childsList.map((child, i) => {
                                 return (
                                   <option
-                                    key={'child_' + i}
+                                    key={"child_" + i}
                                     value={child.childId}
                                   >
-                                    {child.firstName + ' ' + child.lastName}
+                                    {child.firstName + " " + child.lastName}
                                   </option>
-                                )
+                                );
                               })}
                           </BootStrapForm.Select>
                         </BootStrapForm.Group>
                       </div>
                     </span>
                   </div>
-                  {step === 1 ? <Button
-                      className='add-child-btn'
+                  {step === 1 ? (
+                    <Button
+                      className="add-child-btn"
                       onClick={() => setShowAddChildDialog(true)}
-                    > 
-                    Add Child
-                  </Button>
-                  : '' }
+                    >
+                      Add Child
+                    </Button>
+                  ) : (
+                    ""
+                  )}
                 </div>
               )}
-              <div className='content-area-inner internal-page-wrapper'>
+              <div className="content-area-inner internal-page-wrapper">
                 <LeftMenuBar
                   menuItems={sidebarMenuItems}
-                  parentPage='userProfile'
+                  parentPage="userProfile"
                   step={step}
                 />
-                <div className='inner-page-content right'>
-                  <div className='inner-page-right-container'>
-                    <h6 className='student-heading'>{pageTitle}</h6>
-                    <p className='Stud-info'>
+                <div className="inner-page-content right">
+                  <div className="inner-page-right-container">
+                    <h6 className="student-heading">{pageTitle}</h6>
+                    <p className="Stud-info">
                       Please provide accurate details of the student applying
                       for admission. This information is used to help the school
                       best cater for the educational needs of the student.
@@ -224,19 +254,18 @@ export const AdmissionForms = () => {
                         case 1:
                           return (
                             <StudentDetails
-                              currentStudent={currentStudent}
                               selectedChild={selectedChild}
                               setSelectedChild={setSelectedChild}
                               setStep={setStep}
                             />
-                          )
+                          );
                         case 2:
                           return (
                             <MedicalForm
                               selectedChild={selectedChild}
                               setStep={setStep}
                             />
-                          )
+                          );
                         case 3:
                           return (
                             <ExtracurricularForm
@@ -244,7 +273,7 @@ export const AdmissionForms = () => {
                               setSelectedChild={setSelectedChild}
                               setStep={setStep}
                             />
-                          )
+                          );
                         case 4:
                           return (
                             <BackgroundCheckForm
@@ -252,23 +281,23 @@ export const AdmissionForms = () => {
                               setSelectedChild={setSelectedChild}
                               setStep={setStep}
                             />
-                          )
+                          );
                         case 5:
                           return (
                             <ParentsGuardianComponent
                               currentStudent={selectedChild}
                               setStep={setStep}
                             />
-                          )
+                          );
                         case 6:
                           return (
                             <SupportingDocumentForm
                               currentStudent={selectedChild}
                               setStep={setStep}
                             />
-                          )
+                          );
                         default:
-                          break
+                          break;
                       }
                     })()}
                   </div>
@@ -283,6 +312,6 @@ export const AdmissionForms = () => {
         </Container>
       </section>
     </Layout>
-  )
-}
-export default AdmissionForms
+  );
+};
+export default AdmissionForms;
