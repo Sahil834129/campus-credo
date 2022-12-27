d9b83e60133e450f80011b95f788a389280b6b10
Anand <anandvishwakarma83@gmail.com>
2022-12-27 18:15:21 +0530

    facilities in one line, platform fee changes, label fix for closed school fixed

--- src/components/PaymentCard.js
+++ src/components/PaymentCard.js
@@ -1,64 +1,77 @@
 import React, { useState } from "react";
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 import ReviewAdmissionDialog from "../dialogs/reviewAdmissionDialog";
 
-const PaymentCard = ({selectedChild}) => {
+const PaymentCard = ({ selectedChild }) => {
+  const [showReviewApplication, setShowReviewApplication] = useState(false);
+  let totalFee = 0;
+  let convenienceFee = 25;
+  let totalPay = 0;
+  selectedChild.cartItems.forEach((cartItem) => {
+    totalFee += parseFloat(cartItem.admissionFormFee);
+  });
+  let gstAmt = parseFloat((parseFloat(convenienceFee) * 18) / 100).toFixed(2);
+  totalPay =
+    parseFloat(totalFee) + parseFloat(convenienceFee) + parseFloat(gstAmt);
 
-    const [showReviewApplication, setShowReviewApplication] = useState(false);
-    let totalFee = 0;
-    let convenienceFee = 0;
-    let totalPay = 0;
-    selectedChild.cartItems.forEach(cartItem => {
-      totalFee += parseFloat(cartItem.admissionFormFee);
-    });
-    let gstAmt = parseFloat((totalFee + parseFloat(convenienceFee)) * 18/100,).toFixed(2)
-    totalPay = parseFloat(totalFee) + parseFloat(gstAmt);
+  const handleShowReviewApplication = () => setShowReviewApplication(true);
+  const handleCloseReviewApplicationDialog = () => {
+    setShowReviewApplication(false);
+  };
 
-    const handleShowReviewApplication = () => setShowReviewApplication(true)
-    const handleCloseReviewApplicationDialog = () => {
-        setShowReviewApplication(false);
-    }
-    
-    return (
-        <Card className='school-card cart-payment-card'>
-            <div className='card-header-item title'>Order Summary</div>
-            <Card.Body className='school-info-main'>
-                <ListGroup className="info-list-group">
-                    {/* <ListGroup.Item>
+  return (
+    <Card className="school-card cart-payment-card">
+      <div className="card-header-item title">Order Summary</div>
+      <Card.Body className="school-info-main">
+        <ListGroup className="info-list-group">
+          {/* <ListGroup.Item>
                         <div className='cell left'>School Admission Request</div>
                         <div className='cell right'>{selectedChild.cartItems.length}</div>
                     </ListGroup.Item> */}
-                    <ListGroup.Item>
-                        <div className='cell left'>Admission Fee</div>
-                        <div className='cell right'>₹{totalFee}</div>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <div className='cell left'>Platform Fee</div>
-                        <div className='cell right'> ₹{convenienceFee}</div>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <div className='cell left'>GST 18%</div>
-                        <div className='cell right'> ₹{gstAmt}</div>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="total">
-                        <div className='cell left'>Total Payment</div>
-                        <div className='cell right totalpayment'>₹{totalPay.toFixed(2)}</div>
-                    </ListGroup.Item>
-                </ListGroup>
-            </Card.Body>
-            <Card.Body className='button-wrap'>
-                <Card.Link 
-                    className={'btn checkour' + (selectedChild.id ==='' || selectedChild.cartItems.length === 0 ? ' disabled' : '')} 
-                    onClick={handleShowReviewApplication}
-                    >
-                    Review Admission Form <br /> and Submit
-                </Card.Link>
-                <Card.Link href="/schools" className='btn addmore'>Add More Schools</Card.Link>
-            </Card.Body>
-            <ReviewAdmissionDialog show={showReviewApplication} handleClose={handleCloseReviewApplicationDialog} childId={selectedChild.id} />
-        </Card>
-    )
-}
+          <ListGroup.Item>
+            <div className="cell left">Admission Fee</div>
+            <div className="cell right">₹{totalFee}</div>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <div className="cell left">Platform Fee</div>
+            <div className="cell right"> ₹{convenienceFee}</div>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <div className="cell left">GST 18%</div>
+            <div className="cell right"> ₹{gstAmt}</div>
+          </ListGroup.Item>
+          <ListGroup.Item className="total">
+            <div className="cell left">Total Payment</div>
+            <div className="cell right totalpayment">
+              ₹{totalPay.toFixed(2)}
+            </div>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+      <Card.Body className="button-wrap">
+        <Card.Link
+          className={
+            "btn checkour" +
+            (selectedChild.id === "" || selectedChild.cartItems.length === 0
+              ? " disabled"
+              : "")
+          }
+          onClick={handleShowReviewApplication}
+        >
+          Review Admission Form <br /> and Submit
+        </Card.Link>
+        <Card.Link href="/schools" className="btn addmore">
+          Add More Schools
+        </Card.Link>
+      </Card.Body>
+      <ReviewAdmissionDialog
+        show={showReviewApplication}
+        handleClose={handleCloseReviewApplicationDialog}
+        childId={selectedChild.id}
+      />
+    </Card>
+  );
+};
 
 export default PaymentCard;
--- src/components/schoolDetails/SchoolStats.js
+++ src/components/schoolDetails/SchoolStats.js
@@ -1,67 +1,90 @@
 import React from "react";
 
 const SchoolStats = (props) => {
-    const schoolDetails = props.schoolDetails;
-    return (
-        <><h2>Academic Stats</h2><div className='academic-stats-content'>
-            <div className='academic-stats-row'>
-                <div className='cell'>
-                    <div className='title'>Board</div>
-                    <div className='particular'>{schoolDetails.board || 'NA'}</div>
-                </div>
-                <div className='cell'>
-                    <div className='title'>Ownership</div>
-                    <div className='particular'>{schoolDetails.ownership || 'NA'}</div>
-                </div>
-                <div className='cell'>
-                    <div className='title'>School Level</div>
-                    <div className='particular'>{schoolDetails.schoolLevel || 'NA'}</div>
-                </div>
+  const schoolDetails = props.schoolDetails;
+  return (
+    <>
+      <h2>Academic Stats</h2>
+      <div className="academic-stats-content">
+        <div className="academic-stats-row">
+          <div className="cell">
+            <div className="title">Board</div>
+            <div className="particular">{schoolDetails.board || "NA"}</div>
+          </div>
+          <div className="cell">
+            <div className="title">Ownership</div>
+            <div className="particular">{schoolDetails.ownership || "NA"}</div>
+          </div>
+          <div className="cell">
+            <div className="title">School Level</div>
+            <div className="particular">
+              {schoolDetails.schoolLevel || "NA"}
             </div>
-            <div className='academic-stats-row'>
-                <div className='cell'>
-                    <div className='title'>Gender</div>
-                    <div className='particular'>{schoolDetails.gender || 'NA'}</div>
-                </div>
-                <div className='cell'>
-                    <div className='title'>Establishment Year</div>
-                    <div className='particular'>{schoolDetails.yearEstablishedIn || 'NA'}</div>
-                </div>
-                <div className='cell'>
-                    <div className='title'>School Format</div>
-                    <div className='particular'>{schoolDetails.schoolType || 'NA'}</div>
-                </div>
+          </div>
+        </div>
+        <div className="academic-stats-row">
+          <div className="cell">
+            <div className="title">Gender</div>
+            <div className="particular">{schoolDetails.gender || "NA"}</div>
+          </div>
+          <div className="cell">
+            <div className="title">Establishment Year</div>
+            <div className="particular">
+              {schoolDetails.yearEstablishedIn || "NA"}
             </div>
-            <div className='academic-stats-row'>
-                <div className='cell'>
-                    <div className='title'>School Medium</div>
-                    <div className='particular'>{schoolDetails.mediumOfInstruction || 'NA'}</div>
-                </div>
-                <div className='cell'>
-                    <div className='title'>Academic Session</div>
-                    <div className='particular'>{schoolDetails.admissionInfo?.admissionSession || 'NA'}</div>
-                </div>
-                <div className='cell'>
-                    <div className='title'>Student/Teacher Ratio</div>
-                    <div className='particular'>{schoolDetails.studentTeacherRatio || 'NA'}</div>
-                </div>
+          </div>
+          <div className="cell">
+            <div className="title">School Format</div>
+            <div className="particular">{schoolDetails.schoolType || "NA"}</div>
+          </div>
+        </div>
+        <div className="academic-stats-row">
+          <div className="cell">
+            <div className="title">School Medium</div>
+            <div className="particular">
+              {schoolDetails.mediumOfInstruction || "NA"}
             </div>
-            <div className='academic-stats-row'>
-                <div className='cell'>
-                    <div className='title'>School Timings</div>
-                    <div className='particular'>{schoolDetails.schoolTimings || 'NA'}</div>
-                </div>
-                <div className='cell'>
-                    <div className='title'>Admission Status</div>
-                    <div className='particular'>{schoolDetails.admissionInfo?.admissionStatus} for {schoolDetails.admissionInfo?.admissionSession}</div>
-                </div>
-                <div className='cell'>
-                    <div className='title'>Academic Performance</div>
-                    <div className='particular'>{schoolDetails.academicPerformancePerc || 'NA'}</div>
-                </div>
+          </div>
+          <div className="cell">
+            <div className="title">Academic Session</div>
+            <div className="particular">
+              {schoolDetails.admissionInfo?.admissionSession || "NA"}
             </div>
-        </div></>
-    )
-}
+          </div>
+          <div className="cell">
+            <div className="title">Student/Teacher Ratio</div>
+            <div className="particular">
+              {schoolDetails.studentTeacherRatio || "NA"}
+            </div>
+          </div>
+        </div>
+        <div className="academic-stats-row">
+          <div className="cell">
+            <div className="title">School Timings</div>
+            <div className="particular">
+              {schoolDetails.schoolTimings || "NA"}
+            </div>
+          </div>
+          <div className="cell">
+            <div className="title">Admission Status</div>
+            <div className="particular">
+              {schoolDetails.admissionInfo
+                ? schoolDetails.admissionInfo.admissionStatus +
+                  " For " +
+                  schoolDetails.admissionInfo.admissionSession
+                : "NA"}
+            </div>
+          </div>
+          <div className="cell">
+            <div className="title">Academic Performance</div>
+            <div className="particular">
+              {schoolDetails.academicPerformancePerc || "NA"}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SchoolStats;
