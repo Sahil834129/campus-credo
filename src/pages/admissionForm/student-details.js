dd81cf1e77fe808527b372ed51d78d18b716039d
Anand <anandvishwakarma83@gmail.com>
2023-01-05 17:50:28 +0530

    Shalini - student details first time load issue

--- src/pages/admissionForm/student-details.js
+++ src/pages/admissionForm/student-details.js
@@ -139,7 +139,7 @@ export default function StudentDetails({
         toast.error(RESTClient.getAPIErrorMessage(error));
       }
     },
-    [setSelectedChild]
+    [selectedChild]
   );
 
   function getClassFromAge() {
