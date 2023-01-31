0d681d4be84bef8caaeda6fcb9df8548f2f971eb
Akash Pandey <akash.pandey@primewayz.com>
2023-01-31 16:42:31 +0530

    mobile menu integration for responsiveness

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
-import SearchBar from "./SearchBar";
 import MenuBar from "./MenuBar";
+import MobileMenuBar from "./MobileMenuBar";
+import SearchBar from "./SearchBar";
 
 const Header = () => {
     return (
         <header className="header">
-            <MenuBar/>
-            <SearchBar/>
+            <div className="common-header">
+                <MenuBar/>
+                <SearchBar/>
+            </div>
+            
+            <div className="mobile-header">
+                <MobileMenuBar/>
+            </div>
         </header>
+        
     );
 };
 export default Header;
