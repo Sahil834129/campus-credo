4ca66392c9e00a211d7887d82125ddcd30db018f
vipul-gupta <vipulgupta27@gmail.com>
2023-02-02 17:31:03 +0530

    multi role issue fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
+import { injectStore } from "../utils/RestClient";
 
 //const middlewares = [thunk];
 const store = configureStore({
@@ -8,5 +9,7 @@ const store = configureStore({
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
     // {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 });
+
+injectStore(store);
 
 export default store;
