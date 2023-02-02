8bb421b90ae75912c72300b9e28e595838fa78f8
Akash Pandey <akash.pandey@primewayz.com>
2023-02-02 21:04:43 +0530

    homepage, about,faq,school list, signin panel, forgot password ui responsiveness fixed

--- src/common/MobileMenuBar.js
+++ src/common/MobileMenuBar.js
@@ -105,7 +105,7 @@ function handleSelectCity(location) {
         </div>
         <div id="menuToggle"> 
             <input name="toggle" type="checkbox" />
-            <label for="toggle">
+            <label className="toggle-lbl" for="toggle">
                 <span>menu</span>
             
                 <div></div>
@@ -130,31 +130,41 @@ function handleSelectCity(location) {
                     </div>
                 </li>
                 <li className="locations"> 
-                    <Dropdown>
-                        <Dropdown.Toggle as={LocationDropDownToggle} id="dropdown-custom-components">
-                            {selectedLocation ? selectedLocation : "Select"}
-                        </Dropdown.Toggle>
+                    <div className="region-dropdown-wrap">
+                        <i className="loc-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" /></svg>
+                        </i>
+                        <Dropdown>
+                            <Dropdown.Toggle as={LocationDropDownToggle} id="dropdown-custom-components">
+                                {selectedLocation ? selectedLocation : "Select"}
+                            </Dropdown.Toggle>
 
-                        <Dropdown.Menu as={LocationDropDownMenu}>
-                            {
-                                cities.map((city, index) => {
-                                    return <Dropdown.Item key={"city_"+index} eventKey={"city_e_" + index} onClick={ e => handleSelectCity(city)}>{city ? city : ''}</Dropdown.Item>
-                                })
-                            }
-                        </Dropdown.Menu>
-                    </Dropdown>
+                            <Dropdown.Menu as={LocationDropDownMenu}>
+                                {
+                                    cities.map((city, index) => {
+                                        return <Dropdown.Item key={"city_"+index} eventKey={"city_e_" + index} onClick={ e => handleSelectCity(city)}>{city ? city : ''}</Dropdown.Item>
+                                    })
+                                }
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
                 </li>
                 <li className="signin"><LoggedInUserDropDown/></li>            
-                <li>
-                    <Link  onClick={(e) =>
-                            gotoHome(e, navigate) 
-                        }>Home</Link>
-                </li>
-                <li><a href="/aboutUs">About Us</a></li>
-                <li><a href="/schools">All Schools</a></li>
-                <li><a href="/faqs">FAQ's</a></li>
-                <li><a href="/contactUs">Contact Us</a></li>
-                <li className="contact-info">+91-9903096754 / support@campuscredo.com</li>
+                
+                <div className="page-link-wrap">
+                    <li className="page-link">
+                        <Link onClick={(e) =>
+                                gotoHome(e, navigate) 
+                            }>Home</Link>
+                    </li>
+                    <li className="page-link"><a href="/aboutUs">About Us</a></li>
+                    <li className="page-link"><a href="/schools">All Schools</a></li>
+                    <li className="page-link"><a href="/faqs">FAQ's</a></li>
+                    <li className="page-link"><a href="/contactUs">Contact Us</a></li>
+                </div>
+                <div className="contact-info-wrap">
+                    <li className="contact-info">+91-9903096754 / support@campuscredo.com</li>
+                </div>
             </ul>
         </div>
     </nav>
