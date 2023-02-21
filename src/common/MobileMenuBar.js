a5a16f992d8616344acf32df706de46c829aff03
Akash Pandey <akash.pandey@primewayz.com>
2023-02-21 18:27:19 +0530

    ui fixes on qa inputs

--- src/common/MobileMenuBar.js
+++ src/common/MobileMenuBar.js
@@ -129,28 +129,29 @@ function handleSelectCity(location) {
                         </div>
                     </div>
                 </li>
-                <li className="locations"> 
-                    <div className="region-dropdown-wrap">
-                        <i className="loc-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" /></svg>
-                        </i>
-                        <Dropdown>
-                            <Dropdown.Toggle as={LocationDropDownToggle} id="dropdown-custom-components">
-                                {selectedLocation ? selectedLocation : "Select"}
-                            </Dropdown.Toggle>
+                <div className="loc-cartinfo-nav-wrapper">
+                    <li className="locations"> 
+                        <div className="region-dropdown-wrap">
+                            <i className="loc-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" /></svg>
+                            </i>
+                            <Dropdown>
+                                <Dropdown.Toggle as={LocationDropDownToggle} id="dropdown-custom-components">
+                                    {selectedLocation ? selectedLocation : "Select"}
+                                </Dropdown.Toggle>
 
-                            <Dropdown.Menu as={LocationDropDownMenu}>
-                                {
-                                    cities.map((city, index) => {
-                                        return <Dropdown.Item key={"city_"+index} eventKey={"city_e_" + index} onClick={ e => handleSelectCity(city)}>{city ? city : ''}</Dropdown.Item>
-                                    })
-                                }
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
-                </li>
-                <li className="signin"><LoggedInUserDropDown/></li>            
-                
+                                <Dropdown.Menu as={LocationDropDownMenu}>
+                                    {
+                                        cities.map((city, index) => {
+                                            return <Dropdown.Item key={"city_"+index} eventKey={"city_e_" + index} onClick={ e => handleSelectCity(city)}>{city ? city : ''}</Dropdown.Item>
+                                        })
+                                    }
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
+                    </li>
+                    <li className="signin"><LoggedInUserDropDown/></li>            
+                </div>
                 <div className="page-link-wrap">
                     <li className="page-link">
                         <Link onClick={(e) =>
