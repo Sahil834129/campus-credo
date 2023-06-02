002584ced6a020309aaca60e566ad236d0ec1cb7
Vipul Gupta <vipulgupta27@gmail.com>
2023-06-02 16:11:28 +0530

    replace Application number to Application ID

--- src/components/userProfile/AppliedSchools.js
+++ src/components/userProfile/AppliedSchools.js
@@ -216,7 +216,7 @@ const AppliedSchools = ({ application, setApplications }) => {
 
                   <div className="moreinfo-block">
                     <div className="app-id">
-                      <label>Application Number : {application.applicationNumber}</label>
+                      <label>Application ID : {application.applicationNumber}</label>
                     </div>
                     <div className="row-item">
                       <span className="cell-item">Applied To Class:</span>
