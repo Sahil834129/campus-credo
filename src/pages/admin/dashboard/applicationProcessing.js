a4e2c73da4be8b0698d3ae22f43da02ec741baa1
vipul-gupta <vipulgupta27@gmail.com>
2023-06-07 18:07:58 +0530

    code pushed

--- src/pages/admin/dashboard/applicationProcessing.js
+++ src/pages/admin/dashboard/applicationProcessing.js
@@ -1,6 +1,6 @@
 import { ListGroup } from "react-bootstrap";
 
-export default function ApplicationProcessing({applicationProcessing }) {
+export default function ApplicationProcessing({applicationProcessing, admissionSummary }) {
     return (
         <div className='metrics-block mb1'>
             <div className='title-area'>
@@ -11,7 +11,7 @@ export default function ApplicationProcessing({applicationProcessing }) {
                 <ListGroup.Item>
                     <div className='mitem-wrap'>
                         <label className='lbl'>Application Approved</label>{' '}
-                        <span className='value'>{applicationProcessing?.approved || 0}</span>
+                        <span className='value'>{admissionSummary?.totalApproved || 0}</span>
                     </div>
                     <div className='mitem-wrap'>
                         <label className='lbl'>Under Review</label>{' '}
