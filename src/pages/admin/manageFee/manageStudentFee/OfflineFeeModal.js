b2c0dd54c5469995494ab8770b7e125924b1a9a7
Vipul Gupta <vipulgupta27@gmail.com>
2023-07-31 16:44:18 +0530

    issue fixed

--- src/pages/admin/manageFee/manageStudentFee/OfflineFeeModal.js
+++ src/pages/admin/manageFee/manageStudentFee/OfflineFeeModal.js
@@ -148,7 +148,7 @@ export default function OfflineFeeModal({ show, handleClose, student }) {
                             size='sm'
                             type="number"
                             value={lateFeeAmount}
-                            min={1}
+                            min={0}
                             placeholder="Enter Late Fee"
                             style={{ 
                                 backgroundColor: 'white', 
